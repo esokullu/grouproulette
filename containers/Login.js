ff771f8343f9f31cc83893e2075ee7bb169b8a82
Emre Sokullu <esokullu@gmail.com>
2020-02-24 18:14:19 +0300

    added onboarding v1

--- containers/Login.js
+++ containers/Login.js
@@ -1,10 +1,27 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image } from 'react-native';
+//import Image from 'react-native-remote-svg'
+import Onboarding from 'react-native-onboarding-swiper';
 
 const Login = () => {
 
   return (
-    <View />
+    <Onboarding
+      pages={[
+        {
+          backgroundColor: '#fff',
+          image: <Image source={require('../assets/onboard/betterplace.svg')} />,
+          title: 'Onboarding 1',
+          subtitle: 'Done with React Native Onboarding Swiper',
+        },
+        {
+          backgroundColor: '#000',
+          image: <Image source={require('../assets/onboard/betterplace.svg')} />,
+          title: 'Onboarding 2',
+          subtitle: 'Done with React Native Onboarding Swiper',
+        }
+        ]}
+  />
   );
 };
 
